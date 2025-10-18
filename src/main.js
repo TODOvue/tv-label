64c024a43594497e58403a0fb38d91988a4134d3
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-17 21:06:06 -0500

    chore(main): remove unused imports for cleaner entry file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import TvLabel from './demo/Demo.vue'
-import 'github-markdown-css';
-import '../src/assets/scss/_global.scss'
 
 createApp(TvLabel).mount('#tv-label')
