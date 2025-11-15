f083a94fa00ce4c4f89c408e5900cec2a83940b1
Cristhian Daza <cristian.d.b@hotmail.com>
2025-11-15 00:20:35 -0500

    refactor(TvLabel): remove CSS runtime injection and migrate to extracted CSS build for SSR

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import { TvDemo } from '@todovue/tv-demo'
 import TvLabel from './demo/Demo.vue'
+import '@todovue/tv-demo/style.css'
+import './style.scss'
 
-createApp(TvLabel).mount('#tv-label')
+const app = createApp(TvLabel)
+app.component('TvDemo', TvDemo)
+app.mount('#tv-label')
